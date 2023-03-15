5bde40cabf49c24f81b1ba26e66eab455a86a92f
Shalom0117 <shalomalexander707@gmail.com>
2023-03-15 00:28:07 +0000

    add calculateDistance method

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 (function(window, _) {
-    window.myLibraryName = window.myLibraryName || {
-      numz: {},
+    window.shalomRepository = window.shalomRepository || {
+      numz: {
+        /**
+         This function calculates the distnace between two points
+         * @param {object} pointA
+         * @param {object} pointB
+         * @param {number} pointA.x
+         * @param {number} pointA.y
+         * @param {number} pointB.x
+         * @param {number} pointB.y
+         */
+        getAngleDegrees(pointA, pointB){
+          const
+                distanceX = pointB.x - pointA.x,
+                distanceY = pointB.y - pointA.y,
+                radians = Math.atan2(distanceY, distanceX),
+                degrees = radians * 180 / Math.PI;
+          return degrees;
+      },
+
+     /** @param {number} degrees */ 
+      degreesToRadians(degrees) {
+        return degrees * Math.PI / 180;
+      },
+
+      /**@param {number} radians */
+      radiansToDegrees(radians) {
+        return radians * 180 / Math.PI;
+      },
+
+    },
+
       phyz: {
 
         /**
@@ -13,7 +43,7 @@
             const
             distanceX = pointB.x - pointA.x, 
             distanceY = point.y - point.y, 
-            distance = Math.sqrt(distanceX * distanceX + distnaceY * distanceY);
+            distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
             return distance;
         },
       },
